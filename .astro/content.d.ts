d838585811a98b5289ef0dfb5d7963222975b717
Lucien Immink <lucien@arielext.org>
2025-04-23 11:06:17 +0200

    Update content type definitions for talks and adjust content configuration

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -151,7 +151,7 @@ declare module 'astro:content' {
   id: string;
   body?: string;
   collection: "talks";
-  data: any;
+  data: InferEntrySchema<"talks">;
   rendered?: RenderedContent;
   filePath?: string;
 }>;
@@ -160,5 +160,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = never;
+	export type ContentConfig = typeof import("./../src/content.config.js");
 }
